7ee123fc02a9e7f2619c4c90cba0f78b736844ab
juntanaka <edamame.zombie@gmail.com>
2019-11-03 21:51:37 +0900

    課題 完成 小数点第2以下切り捨て実装

--- Calculate.py
+++ Calculate.py
@@ -3,7 +3,7 @@ class Circle:
         self.radius = radius
 
     def area(self):
-        return (self.radius ** 2 * 3.14) / 2
+        return self.radius ** 2 * 3.14
 
     def perimeter(self):
         return (self.radius * 2) * 3.14
@@ -12,3 +12,7 @@ class Circle:
 circle1 = Circle(radius=1)
 print(circle1.area())
 print(circle1.perimeter())
+
+circle3 = Circle(radius=3)
+print(circle3.area())
+print(circle3.perimeter())
